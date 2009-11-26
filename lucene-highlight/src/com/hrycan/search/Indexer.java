500b56b11b491e669a13edb6f873be8964a6c8a4
nick.hrycan <nick.hrycan@3c5264ae-c1ad-11de-8ec6-755e00613368>
2009-11-26 13:24:45 +0000

    added code for update lucene document blog post

--- lucene-highlight/src/com/hrycan/search/Indexer.java
+++ lucene-highlight/src/com/hrycan/search/Indexer.java
@@ -17,10 +17,10 @@ import org.apache.lucene.store.FSDirectory;
  * Simple Lucene indexer class to build an index to use with the Searcher
  * in order to demonstrate the Lucene Highlighter
  * 
- * usage: Indexer <index dir> <data dir>
+ * usage: Indexer indexdir datadir
  * 
- * index dir: where the index will be stored
- * data dir: dir containing plain txt files
+ * indexdir: where the index will be stored
+ * datadir: dir containing plain txt files
  */
 public class Indexer {
 	protected static Logger log = Logger.getLogger(Indexer.class);
@@ -34,28 +34,44 @@ public class Indexer {
 		
 		String indexDir = args[0];
 		String dataDir = args[1];
-		
+		IndexWriter writer = null;
+		int numDocsIndexed = 0;
 		long start = System.currentTimeMillis();
 		
-		Directory dir = FSDirectory.getDirectory(new File(indexDir));
-		IndexWriter writer = new IndexWriter(dir, new StandardAnalyzer(), true, IndexWriter.MaxFieldLength.UNLIMITED);
-		
-		File[] files = new File(dataDir).listFiles();
-		for (File file : files) {
-			if (file.getName().endsWith(".txt")) {
-				Document doc = new Document();
-				String contents = readFile(file);
-				doc.add(new Field("contents", contents, Field.Store.COMPRESS, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
-				doc.add(new Field("filepath", file.getCanonicalPath(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-				doc.add(new Field("filename", file.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-				writer.addDocument(doc);     
+		try {
+			Directory dir = FSDirectory.getDirectory(new File(indexDir));
+			writer = new IndexWriter(dir, new StandardAnalyzer(), true, IndexWriter.MaxFieldLength.UNLIMITED);
+			
+			File[] files = new File(dataDir).listFiles();
+			for (File file : files) {
+				if (file.getName().endsWith(".txt")) {
+					Document doc = new Document();
+					
+					String fileContent = readFile(file);
+					if (fileContent != null ) {
+						String[] parsed = fileContent.split("\\n", 4);
+						if (parsed.length == 4) {
+							doc.add(new Field("author", parsed[0], Field.Store.YES, Field.Index.NOT_ANALYZED));
+							doc.add(new Field("date", parsed[1], Field.Store.YES, Field.Index.NOT_ANALYZED));
+							doc.add(new Field("source", parsed[2], Field.Store.YES, Field.Index.NOT_ANALYZED));
+							doc.add(new Field("contents", parsed[3], Field.Store.COMPRESS, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+							doc.add(new Field("filepath", file.getCanonicalPath(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+							doc.add(new Field("filename", file.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+							
+							writer.addDocument(doc);
+						}
+					}
+				}
+			}
+			
+			numDocsIndexed = writer.numDocs();
+		}
+		finally {
+			if (writer != null) {
+				writer.optimize();
+				writer.close();
 			}
 		}
-		
-		int numDocsIndexed = writer.numDocs();
-		
-		writer.optimize();
-		writer.close();  
 		
 		long total = System.currentTimeMillis() - start;
 		log.info("Indexing complete: indexed " + numDocsIndexed + " files in " + total + "ms.  " +
@@ -63,6 +79,19 @@ public class Indexer {
 		
 	}
 	
+	/**
+	 * Format of the plain text file is as follows:<br/>
+	 * line 1: author <br/>
+	 * line 2: date <br/>
+	 * line 3: source <br/>
+	 * remainder of the file is considered the content
+	 * 
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 * 
+	 
+	 */
 	public static String readFile(File file) throws Exception {
 		String result = "";
 		FileInputStream fin = new FileInputStream(file);
